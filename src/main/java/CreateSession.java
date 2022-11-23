d88e917bdcb842e5457fc9e96701cba1a299201e
MrEmmanuel <sejesoo@gmail.com>
2022-11-23 22:58:24 +0200

    refectored main class

---
+++ src/main/java/CreateSession.java
@@ -0,0 +1,33 @@
+import javax.mail.*;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Random;
+
+public class CreateSession {
+
+    Random random = new Random();
+
+    public Message createSession(String server, String port, String login, String password, String to) throws MessagingException, FileNotFoundException {
+
+        Properties properties = CreateMessage.setProperties(server, port, login, password);
+        Session session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(login, password);
+            }
+        });
+        session.setDebug(true);
+        Quotes quotes = new Quotes("quotes.txt");
+        quotes.readQuotesFromFile();
+        List<String> newQuotes = quotes.getQuotes();
+        int number = random.nextInt(11);
+        String quote = newQuotes.get(number);
+
+        String subject = "Random Inspirational Quotes.";
+        String[] information = new String[]{login, to, subject, quote};
+        Message message = CreateMessage.createMessage(session, information);
+
+        return message;
+    }
+}
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -9,33 +9,18 @@ import java.util.Random;
 
 public class MainClass {
     public static void main(String[] args) throws FileNotFoundException, MessagingException {
-        Random random = new Random();
+
         SendEmail email = new SendEmail();
+        CreateSession session = new CreateSession();
+
         Map<String, String> env = System.getenv();
         String login = env.get("SMTP_LOGIN");
         String server = env.get("SMTP_SERVER");
         String port = env.get("SMTP_PORT");
         String password = env.get("SMTP_PASSWORD");
+        String emailAddress = args[0];
 
-        Properties properties = CreateMessage.setProperties(server, port, login, password);
-        Session session = Session.getInstance(properties, new Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(login, password);
-            }
-        });
-        session.setDebug(true);
-
-        Quotes quotes = new Quotes("quotes.txt");
-        quotes.readQuotesFromFile();
-        List<String> newQuotes = quotes.getQuotes();
-        int number = random.nextInt(11);
-        String quote = newQuotes.get(number);
-
-        String to = args[0];
-        String subject = "Random Inspirational Quotes.";
-        String[] information = new String[]{login, to, subject, quote};
-        Message message = CreateMessage.createMessage(session, information);
-
+        Message message = session.createSession(server, port, login, password, emailAddress);
         EmailRandomInspirationalQuote randomEmail = new EmailRandomInspirationalQuote(email);
         randomEmail.sendEmail(message);
     }
